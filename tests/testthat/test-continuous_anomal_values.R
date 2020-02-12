97aaf9128e409f6f72e94e79006c314cab8a0505
Daniele <daniele.bottigliengo@libero.it>
2020-02-12 14:15:10 +0100

    Add test to continuous_anomal_values

--- tests/testthat/test-continuous_anomal_values.R
+++ tests/testthat/test-continuous_anomal_values.R
@@ -1,6 +1,17 @@
-context("test-continuous_anomal_values")
 
 test_that("Class of output is correct", {
+
+    x <- rnorm(n = 10L, mean = 10, sd = 2)
+    range   <- c(3, 12)
+
+    expect_is(
+        continuous_anomal_values(x = x, range = range),
+        class = "numeric"
+    )
+
+})
+
+test_that("NA if all the values are inside the range", {
 
     x <- rnorm(n = 10L, mean = 10, sd = 2)
     range   <- c(3, 12)
