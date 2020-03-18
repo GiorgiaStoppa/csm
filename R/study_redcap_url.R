88a2436b5059068f02609d6fe1400ef9bfb9c705
Daniele Bottigliengo <daniele.bottigliengo@libero.it>
2020-03-18 11:03:28 +0100

    Add `study_redcap_url`

---
+++ R/study_redcap_url.R
@@ -0,0 +1,10 @@
+#' Return the REDCap's url of the study
+#'
+#' @return (chr) the REDCap url to access the study's data.
+#' @export
+#'
+#' @examples
+#' study_redcap_url()
+study_redcap_url <- function() {
+    "https://redcap.dctv.unipd.it/api/"
+}
---
+++ tests/testthat/test-study_redcap_url.R
@@ -0,0 +1,7 @@
+
+test_that("study_redcap_url works", {
+
+    expect_is(study_redcap_url(), "character")
+
+})
+
