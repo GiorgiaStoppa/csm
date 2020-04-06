e588ae70eca3d9ed42e75894221383d68abbdebc
Daniele Bottigliengo <daniele.bottigliengo@libero.it>
2020-04-06 19:35:23 +0200

    Add the function `remove_last_number`

---
+++ R/remove_last_number.R
@@ -0,0 +1,25 @@
+#' Remove last numbers
+#'
+#' The function takes a character vector and it removes the last two
+#' positions of the element, if the an element ends with a "_" and a
+#' number.
+#'
+#' @param x (chr) a character vector
+#'
+#' @return (chr) a character vector
+#' @export
+#'
+#' @examples
+#'
+#' a <- c("aa", "bb", "cc", "dd_1", "dd_2", "ff")
+#'
+#' remove_last_number(x = a)
+#'
+
+remove_last_number <- function(x) {
+
+    assertive::assert_is_character(x)
+
+    stringr::str_remove(x, pattern = "_[0-9]+$")
+}
+
